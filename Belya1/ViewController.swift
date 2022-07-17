cf9c0d4c10dbd675d9cdcba41a660aafde8a08e1
abdallah8399 <87143893+abdallah8399@users.noreply.github.com>
2022-07-17 15:29:21 +0200

    Add files via upload

---
+++ Belya1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Belya1
+//
+//  Created by Aly Code on 17/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Belya1UITests/Belya1UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Belya1UITestsLaunchTests.swift
+//  Belya1UITests
+//
+//  Created by Aly Code on 17/07/2022.
+//
+
+import XCTest
+
+class Belya1UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
